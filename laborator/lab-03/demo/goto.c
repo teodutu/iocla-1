560becb49bfae92f56d245bcfda7024aa6ecd8bf
teodutu <teodor.dutu@gmail.com>
2020-10-26 13:13:13 +0200

    [Lab 3] Add demo

---
+++ laborator/lab-03/demo/goto.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int cond = 1, cond1 = 1;
+	goto ceva;
+
+	// cod -> nu se executa
+	printf("Nu se executa");
+
+ceva:
+	printf("Se executa\n");
+
+	// if cu goto
+	if (cond) {
+		// ceva
+	} else {
+		// altceva
+	}
+
+	if (!cond)
+		goto else_body;
+
+	// ceva
+
+else_body:
+	// altceva
+
+// 	for (ceva) {
+// 		for (alteceva) {
+// 			for (blabla) {
+// 				if (caca)
+// 					goto exit_loops;
+// 			}
+// 		}
+// 	}
+// exit_loops:
+
+	// while cu goto
+	while (cond) {
+		// ceva
+	}
+
+whie_body:
+	if (!cond)
+		goto exit_while;
+
+	// ceva
+	goto whie_body;
+
+exit_while:
+
+	// orice
+
+	// NU ASA. Folositi de Morgan
+	if (cond1 && cond)
+		goto exit;
+	if (cond1 || cond)
+		goto exit;
+
+	// ex. de Morgan:
+	!(cond && cond1) == !cond || !cond1;
+	if (!cond1)
+		goto exit;
+	if (!cond)
+		goto exit;
+
+exit:
+	return 0;
+}
