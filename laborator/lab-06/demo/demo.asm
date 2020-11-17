e2d6bd0f3f592e03e5598bbd1533ee45376fbae6
teodutu <teodor.dutu@gmail.com>
2020-11-17 19:36:42 +0200

    [Lab 6] Improve demo

--- laborator/lab-06/demo/demo.asm
+++ laborator/lab-06/demo/demo.asm
@@ -14,30 +14,31 @@ main:
 	mov eax, 2
 	mov ebx, 3
 
-	push eax  ; pune eax pe stiva
-	; <=> sub esp, 4; mov [esp], eax
+	; xchg eax, ebx
+	push eax  ; <=> sub esp, 4; mov [esp], eax
 	push ebx
-	; siva:
+	; stiva:
 	;	eax
 	;	ebx
 
-	pop eax
-	; mov eax, [esp]; add esp, 4
+	pop eax  ; <=> mov eax, [esp]; add esp, 4
 	pop ebx
 
 	PRINTF32 `eax = %d | edx = %d\n\x0`, eax, ebx
 
+	; nu stie push < word
+	; pt ca initial era gandit pt 16b si push byte strica alinierea
+	push byte 1  ; face de fapt push dword 1
+
 	sub esp, 2
-	mov [esp], word 127
+	mov [esp], word 65  ; 65 = 0x0041 -> 0x41 0x00 (l endian)
 
-	sub esp, 4
-	mov [esp], dword "Gigi"
+	push dword "asdf"
 
-	; NU afisati esp ca il strica PRINTF32
+	; NU afisati esp; il strica PRINTF32
 	mov eax, esp
-	PRINTF32 `stiva = %s\n\x0`, eax
+	PRINTF32 `%s\n\x0`, eax
 
-	; la iesirea din functie stiva trebuie sa fie la fel ca la intrare
-	add esp, 6
+	add esp, 10  ; dword + word + dword = 10B
 
-    ret
+    ret  ; <=> pop eip
