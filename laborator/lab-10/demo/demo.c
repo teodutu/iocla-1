59c1a8222d857d25f0faa57a2d894abcc284d50f
teodutu <teodor.dutu@gmail.com>
2021-01-11 14:42:30 +0200

    [Lab 10] Add late demo

---
+++ laborator/lab-10/demo/demo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void rip_dani_mocanu()
+{
+	printf(
+		"Dani Mocanu s-a lasat de manele :(((\n"
+		"https://www.youtube.com/watch?v=hp-MjbsKQkM\n"
+	);
+}
+
+
+void read_data()
+{
+	int x = 2;
+	char s[32];
+	x = 3;
+
+	printf("RIP Dani Mocanu!\n");
+	scanf("%s", s);
+}
+
+int main(void)
+{
+	read_data();
+	return 0;
+}
