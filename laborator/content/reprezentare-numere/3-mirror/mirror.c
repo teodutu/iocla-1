d2619f4ef5a56033ce18ba85ef06f2ce4709afd0
Teodor Dutu <teodor.dutu@gmail.com>
2021-10-15 15:18:56 +0300

    [Lab 1] Replace AnaAreMere with manele
    Signed-off-by: Teodor Dutu <teodor.dutu@gmail.com>

--- laborator/content/reprezentare-numere/3-mirror/mirror.c
+++ laborator/content/reprezentare-numere/3-mirror/mirror.c
@@ -11,23 +11,28 @@ void mirror(char *s)
 int main()
 {
 	/* TODO: Test function */
-	// Sirurile "AnaAreMere" sunt plasate intr-o zona de date READ-ONLY
+	// "manele" se stocheaza intr-o zona de date READ-ONLY
 	// ASA NU
-	// char *str1 = "AnaAreMere";
-	// mirror(str1);
+	mirror("manele");
 
-	// ASA NU
-	// mirror("AnaAreMere");
+	char *s = "manele";
+	mirror(s);
 
-	// Sirurile de mai jos sunt puse pe stiva (READ-WRITE)
-	char str2[] = "AnaAreMere";
-	char str3[11] = "AnaAreMere";
-	
-	mirror(str2);
-	// printf("str2 = %s\n", str2);
+	// s1 si s2 se stocheaza pe stiva: READ-WRITE
+	// ASA DA
+	char s1[] = "manele";
+	char s2[7] = "manele";
 
-	mirror(str3);
-	// printf("str3 = %s\n", str3);
+	mirror(s1);
+	mirror(s2);
+
+	// s3 se stocheza pe heap: READ-WRITE
+	// e ok sizeof-ul asta. sizeof se evalueaza la compilare => nu se aloca nimic pt "manele"
+	// sizeof("manele") == 7; contine si \0
+	// ASA DA (dar nu-i eficient sa aloci si memorie pe heap pt ceva ce poate fi tinut pe stiva)
+	char *s3 = malloc(sizeof("manele"));
+	strcpy(s3, "manele");
+	mirror(s3);
 
 	return 0;
 }
