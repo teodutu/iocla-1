1d0ed731f7cbaad4d0ddf5a2cf0019f926608a82
Teodor Dutu <teodor.dutu@gmail.com>
2021-10-22 13:03:59 +0300

    [Lab 2] Extend demo for most process sections
    Signed-off-by: Teodor Dutu <teodor.dutu@gmail.com>

--- laborator/content/operatii-memorie-gdb/demo/demo_ptr.c
+++ laborator/content/operatii-memorie-gdb/demo/demo_ptr.c
@@ -2,44 +2,62 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int global[100];
+// Pointer in .data; string in .rodata
+char *global_ct_str = "manele";
 
-int *f() {
-	int x = 10;
-	// Compilatorul se prinde ca e prost ce fac => schimba
-	// codul ca sa return NULL
-	return &x;
-}
+// .data
+// Desi au aceeasi valoare, stringurile sunt zone diferite de memorie,
+// pentru ca nu sunt constante 
+char global_str1[] = "florin salam";
+char global_str2[] = "florin salam";
 
-int main(int argc, char const *argv[])
+int main(int argc, char const **argv)
 {
-	int a = 10;
+	// https://i.stack.imgur.com/9ZHwD.png
 
-	// Pt compilator: stack ~ label pt zona de memorie
-	int stack[100];
-	printf("&stack = %p\n", &stack);
-	printf("stack = %p\n", stack);
+	int a = 10; // pe stiva
+	printf("&a = %p\n", &a);
+	printf("a = %d\n\n", a);
 
-	printf("&global = %p\n", &global);
-	printf("global = %p\n", global);
+	printf("&global_ct_str = %p\n", &global_ct_str);
+	printf("global_ct_str = %p\n\n", global_ct_str);
 
-	// SO ii da unui proces memorie (cel putin cata cere)
-	// SO aloca mult mai mult heap decat 1 byte
-	int *p = malloc(1);
-	*p = 2;
-	printf("&p = %p\n", &p);
-	printf("p = %p\n", p);
-	printf("*p = %d\n", *p);
+	global_ct_str = "florin salam";
+	printf("global_ct_str = %p\n\n", global_ct_str);
 
-	// int *main_ptr(int, const char **) == declarare de functie
-	int (*main_ptr)(int, const char **) = main;  // sau &main
-	printf("&main_ptr = %p\n", &main_ptr);
-	printf("main_ptr = %p\n", main_ptr);
+	// Pointer pe stiva, string in .rodata
+	char *stack_ct_str = "manele";
+	printf("&stack_ct_str = %p\n", &stack_ct_str);
+	printf("stack_ct_str = %p\n\n", stack_ct_str);
 
-	printf("%p\n", f());
+	printf("&global_str1 = %p\n", &global_str1);
+	printf("global_str1 = %p\n\n", global_str1);
+	printf("&global_str2 = %p\n", &global_str2);
+	printf("global_str2 = %p\n\n", global_str2);
 
-	sleep(1);
-	(*main_ptr)(0, NULL);
+	// Array-urile statice sunt doar labeluri pt zona de memorie
+	// "florin salam" e pus pe stiva
+	char stack_str[] = "florin salam";
+	printf("&stack_str = %p\n", &stack_str);
+	printf("stack_str = %p\n\n", stack_str);
+
+	int *heap_ptr = malloc(1);  // aloc 1 byte
+	// SO aloca mai mult heap la inceputul rularii => nu da seg fault.
+	heap_ptr[1000] = 2;
+	printf("&heap_ptr = %p\n", &heap_ptr);
+	printf("heap_ptr = %p\n", heap_ptr);
+	printf("heap_ptr[1000] = %d\n\n", heap_ptr[1000]);
+
+	int (*mai_ptr)(int, char const **) = main;
+	printf("&main_ptr = %p\n", &mai_ptr);
+	printf("main_ptr = %p\n\n", mai_ptr);
+
+	printf("printf = %p\n\n", printf);
+
+	sleep(5);
+	mai_ptr(0, NULL);
+
+	// sleep(1000);
 
 	return 0;
 }
