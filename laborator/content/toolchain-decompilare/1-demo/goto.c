6cf8e401ed3478a277177d6134f4cbae168f7394
Teodor Dutu <teodor.dutu@gmail.com>
2021-11-12 21:28:07 +0200

    [Lab 3] Slightly improve demo layout
    Signed-off-by: Teodor Dutu <teodor.dutu@gmail.com>

--- laborator/content/toolchain-decompilare/1-demo/goto.c
+++ laborator/content/toolchain-decompilare/1-demo/goto.c
@@ -11,7 +11,7 @@ int main(void)
 ceva:
 	printf("Se executa\n");
 
-/*------------------------------------------*/
+/*---------------------------------------------*/
 	// if cu goto
 	if (cond1)
 		printf("if body\n");
@@ -21,7 +21,6 @@ ceva:
 	if (!cond1)
 		goto else_body;
 	printf("if body\n");
-
 	goto exit_if;
 
 else_body:
@@ -29,40 +28,41 @@ else_body:
 
 exit_if:
 
-/*------------------------------------------*/
+/*---------------------------------------------*/
 	cond1 = 2;
-	
+
 	// while cu goto
 	while (cond1) {
 		printf("while body\n");
 		--cond1;
 	}
+
 	// <=>
+
 	cond1 = 2;
-while_body:
+while_loop:
 	if (!cond1)
 		goto exit_while;
 
 	printf("while body\n");
-	--cond1;
 
-	goto while_body;
+	--cond1;
+	goto while_loop;
 
 exit_while:
 
-/*------------------------------------------*/
+/*---------------------------------------------*/
 	cond1 = 1;
 	// ASA NU
 	if (cond1 && cond2)
 		printf("cond1 && cond2\n");
-
+	
 	// ASA DA
-	// folosim de Morgan
-	// !(cond1 && cond2) == !cond! || !cond2
+	// cu de Morgan: !(cond1 && cond2) == (!cond1 || !cond2)
 	if (!cond1)
 		goto exit;
 	if (!cond2)
-		goto exit;
+		goto exit; 
 
 	printf("cond1 && cond2\n");
 
