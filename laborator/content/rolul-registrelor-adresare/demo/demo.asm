9ff17515b730c6974fa7abbf70cf43de0deb64bc
teo <teodor.dutu@gmail.com>
2021-11-19 13:05:25 +0200

    [Lab 6] Mix with last year's demo
    Signed-off-by: teo <teodor.dutu@gmail.com>

--- laborator/content/rolul-registrelor-adresare/demo/demo.asm
+++ laborator/content/rolul-registrelor-adresare/demo/demo.asm
@@ -1,26 +1,22 @@
 %include "../utils/printf32.asm"
 
 ; var globale INITIALIZATE
-; int a_dword = 2;
-; short a_word = 3;
-; uint8_t a_byte = 65;
-section .data  ; rw- -> gestionate de SO (pot fi schimbate de mana)
+section .data       ; rw-
     ; db = define byte -> 8b
     ; dw -> word = 16b
     ; dd -> double word = 32b
     ; dq -> quad word = 64b
-    s db "Dorel", 0  ; 0 == '\0'
-    ; <=> s db 'D', 'o', 'r', 'e', 'l', 0
-    a_dword dd 2
-    a_word dw 3
-    a_char db 'A'
+    a_word dw 0x1337
     a_byte db 65
-    ; a resd 3  ; echivalent cu a-l declara in bss
-    v dd 1, 2, 3, 4, 5
+    a_char db 'B'
+    a_string db 'manele', 10, 0     ; 10 = \n in ASCII; 0 = \0
+    ; <=> a_string db 'm', 'a', 'n', 'e', 'l', 'e', 10, 0
+    a_dword dd 10000000
 
 ; var globale CONSTANTE
-section .rodata  ; r--
+section .rodata     ; r--
 ; fix la fel ca .data
+    ints dd 10, 32, 11, 20
 
 ; var globale NEINITIALIZATE;
 ; int a;  // a = 0;
@@ -31,7 +27,7 @@ section .bss  ; rw-
     ; resw, resq
 
 ; cod executabil
-section .text  ; r-x
+section .text       ; r-x
 
 extern printf
 global main
@@ -40,44 +36,7 @@ main:
     push ebp
     mov ebp, esp
 
-    mov eax, a_dword  ; a_dword = ptr la un int din .data care e = 2
-    PRINTF32 `eax = 0x%x\n\x0`, eax
-
-    mov eax, [a_dword]  ; [a_dword] = *(int *)a_dword
-    PRINTF32 `[a_word] = 0x%x\n\x0`, eax
-
-    ; memoria = [ 0x05 | 0x00 | 0x41(a_byte) | 0x41(a_char) ]
-    mov eax, [a_word]  ; operanzii au aceeasi dimensiune
-    ; => eax = 0x41410005
-    PRINTF32 `eax = 0x%x\n\x0`, eax
-
-    mov [a_word], word 20  ; ca si cum castam la word
-    PRINTF32 `[a_word] = %hd\n\x0`, [a_word]
-
-    mov ecx, 5
-    xor eax, eax  ; int i = 0;
-
-print_v:
-    ; v[i] = v[eax] = *(v + eax * sizeof(*v))
-    PRINTF32 `%d \x0`, [v + eax * 4]
-    ; adresare bazata indexata si scalata (denumire cretina; nu va trebuie la lab)
-    inc eax
-    cmp eax, ecx
-    jne print_v
-
-    PRINTF32 `\n\x0`
-
-print_v_loop:
-    PRINTF32 `%d \x0`, [v + (ecx - 1) * 4]  ; <=> [v + ecx * 4 - 4]
-    ; adresare bazata indexata, scalata si cu deplasament (alta denumire idioata)
-    loop print_v_loop
-    ; loop label <=>
-    ;   --ECX
-    ;   if (ECX) goto label
-
-    PRINTF32 `\n\x0`
-
-    ; Inmultirea
+    ; Inmultire
     ; R = OP1 * OP2
     ; 32b * 32b <= 64
     ; mul op2 -> reg/mem
@@ -85,27 +44,74 @@ print_v_loop:
     ;   OP2 = 8b => OP1 = AL; R = AX
     ;   OP2 = 16b => OP1 = AX; R = DX:AX (high = DX; low = AX)
     ;   OP2 = 32b => OP1 = EAX; R = EDX:EAX (high = EDX; low = EAX)
-    mov ax, [a_word]
-    mov bx, 3
-    mul bx
-    PRINTF32 `ax = %d\n\x0`, eax  ; PRINTF32 DOAR CU OPERANZI DE 32b
+    mov bx, 2000
+    mov ax, 1000
+    mul bx      ; rezultat = DX:AX
 
-    mov eax, 2
-    mov ebx, 3
-    imul eax, ebx  ; eax *= ebx
-    PRINTF32 `eax = %d\n\x0`, eax
+    shl edx, 16
+    mov dx, ax
 
-    ; Impartirea cu REST
+    PRINTF32 `edx = %d\n\x0`, edx
+
+    ; Impartire cu REST
     ; D = I * C + R
     ; div I -> reg/mem
     ; C si R se pun si D se ia in fct de I
     ;   I = 8b => D = AX; C = AL; R = AH
     ;   I = 16b => D = DX:AX; C = AX; R = DX
     ;   I = 32b => D = EDX:EAX; C = EAX; R = EDX
-    mov ax, 5
-    mov bl, 2
-    div bl
-    PRINTF32 `C,R = %hx\n\x0`, eax
+    mov ax, 16
+    mov bl, 5
+    div bl      ; R = AH; C = AL
+
+    PRINTF32 `C = %hhd\n\x0`, eax
+    shr ax, 8
+    PRINTF32 `R = %hhd\n\x0`, eax
+
+    ; --------------------------------------------
+    ; Adresare:
+    mov eax, a_dword
+    PRINTF32 `a_dword = 0x%x\n\x0`, eax
+
+    mov eax, [a_dword]
+    PRINTF32 `*a_dword = %d\n\x0`, eax
+
+    mov eax, [a_word]
+    PRINTF32 `*a_word = %x\n\x0`, eax
+
+    ; memoria = [ 0x37 | 0x13 | 0x41(a_byte) | 0x42(a_char) ]
+    mov [a_word], word 10
+    PRINTF32 `*a_word = %hd\n\x0`, [a_word]
+
+    mov ecx, 4
+
+    lea eax, [ints + (ecx - 1) * 4]     ; lea calculeaza
+    PRINTF32 `*eax = %d\n\x0`, [eax]
+
+    mov ecx, 4
+    xor eax, eax  ; int i = 0;
+
+print_ints:
+    ; v[i] = v[eax] = *(ints + eax * sizeof(*v))
+    PRINTF32 `%d \x0`, [ints + eax * 4]
+    ; adresare bazata, indexata si scalata (denumire cretina; nu va trebuie la lab)
+    inc eax
+    cmp eax, ecx
+    jne print_ints
+
+    PRINTF32 `\n\x0`
+
+print_ints_loop:     ; ecx = [4..1]
+    ; ints[ecx] = *(ints + ecx * sizeof(*ints))
+    ; adresare bazata, indexata, scalata cu deplasament (limbaj de lemn...)
+    mov edx, [ints + (ecx - 1) * 4]
+    PRINTF32 `%d \x0`, edx
+    loop print_ints
+    ; loop label <=>
+    ;   dec ECX
+    ;   jnz label
+
+    PRINTF32 `\n\x0`
 
     leave
     ret
