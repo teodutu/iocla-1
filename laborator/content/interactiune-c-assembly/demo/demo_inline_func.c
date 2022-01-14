b6ed7d62d2553a14b6a72efe0486594c4f67a057
Teodor Dutu <teodor.dutu@gmail.com>
2022-01-14 09:18:13 +0200

    [Lab 10] Add true inline function example
    Signed-off-by: Teodor Dutu <teodor.dutu@gmail.com>

--- laborator/content/interactiune-c-assembly/demo/demo_inline_func.c
+++ laborator/content/interactiune-c-assembly/demo/demo_inline_func.c
@@ -2,21 +2,27 @@
 
 // Nu functioneaza din cauza ca nu se defineste un simbol pentru max_inline
 // Cititi sectiunea "ISO C inline semantics" din linkul de mai jos pentru detalii
-// https://runtimeverification.com/blog/undefined-c-common-mistakes/
+// https://runtimeverification.com/blog/undefined-c-common-mistakes/.
 // inline int max_inline(int a, int b)
 // {
 // 	return a > b ? a : b;
 // }
 
 // Functioneaza pentru ca `static` restrange vizibilitatea functiei la fisierul
-// curent, ceea ce forteaza compilatorul sa defineasca un simbol pentru functie
+// curent, ceea ce forteaza compilatorul sa defineasca un simbol pentru functie.
 static inline int max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+// E intotdeauna inline-uita.
+static int __attribute__((always_inline)) max_inline(int a, int b)
 {
 	return a > b ? a : b;
 }
 
 int main(void)
 {
-	printf("max(2, 5) = %d\n", max(2, 5));
+	printf("max(2, 5) = %d\n", max_inline(2, 5));
 	return 0;
 }
